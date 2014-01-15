33d54fe3f0c19715c28a882ba01e9f91142779b7
minimus <ashlebede@gmail.com>
2014-01-15 13:54:18 -0500

    directory structure changed

--- fov/test/test_fov.py
+++ fov/test/test_fov.py
@@ -3,25 +3,28 @@ import unittest
 import curses
 
 class TestFoV (unittest.TestCase):
-    def setUp ():
-        screen = open_screen ()
+    def setUp (self):
+        self.screen = open_screen ()
 
-    def test_fov:
-        screen.addstr (3, 3, "hello world")
-        screen.getch ()
+    def test_fov (self):
+        self.screen.addstr (3, 3, "hello world")
+        self.screen.getch ()
 
-    def open_screen ():
-        screen = curses.initscr ()
-        curses.noecho ()
-        curses.cbreak ()
-        screen.keypad (1)
-        return screen
+    def tearDown (self):
+        close_screen (self.screen)
 
-    def close_screen (screen):
-        curses.nocbreak ()
-        screen.keypad (0)
-        curses.echo ()
-        curses.endwin ()
+def open_screen ():
+    screen = curses.initscr ()
+    curses.noecho ()
+    curses.cbreak ()
+    screen.keypad (1)
+    return screen
+
+def close_screen (screen):
+    curses.nocbreak ()
+    screen.keypad (0)
+    curses.echo ()
+    curses.endwin ()
 
 if __name__ == '__main__':
     unittest.main ()
